404741a264266a0c2a3e2c633e82ab1dbd841f11
Dravid Vaishnav <105485955+DravidVaishnav@users.noreply.github.com>
2023-07-07 17:59:46 +0530

    Update check_sorted.py

--- List/check_sorted.py
+++ List/check_sorted.py
@@ -9,7 +9,7 @@ def check_sorted(lst):
     for i in range(len(lst)-1):
         if lst[i] > lst[i+1]:
             return False 
-        return True
+    return True
 
 # using sorted func
 def issorted(lst):
